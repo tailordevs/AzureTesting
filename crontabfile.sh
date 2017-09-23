a37831308cf334264f9257c187092c90110b5fbf
tailordevs <devendra.t@dsyh.in>
2017-09-23 17:05:37 +0530

    crontab changes

--- crontabfile.sh
+++ crontabfile.sh
@@ -4,15 +4,15 @@
 # http://stackoverflow.com/questions/4880290/how-do-i-create-a-crontab-through-a-script
 # http://stackoverflow.com/questions/14450866/search-for-a-cronjob-with-crontab-l/14451184#14451184
 
-cd /var
-mkdir www
-chmod 777 /www
-chmod 777 /var/www
+#cd /var
+#mkdir www
+#chmod 777 /www
+#chmod 777 /var/www
 
 # apt-get install -y git
-cd /var/www/
-git init
-git remote add origin https://github.com/desairavi1101/Train-Application.git	
+#cd /var/www/
+#git init
+#git remote add origin https://github.com/desairavi1101/Train-Application.git	
 
 JOB='*/1 * * * * /bin/sh -c 'cd /var/www/ && git pull origin master' > /var/www/autodeploy.txt'
 
