6d5a2eba9eda80352d6ba0bd1f82f508081c5cb5
tailordevs <devendra.t@dsyh.in>
2017-09-23 16:52:44 +0530

    crontabfile changes

--- crontabfile.sh
+++ crontabfile.sh
@@ -9,6 +9,9 @@ mkdir www
 chmod 777 /www
 chmod 777 /var/www
 
+apt-get install -y git
+git init
+git remote add origin https://github.com/desairavi1101/Train-Application.git
 
 JOB='*/1 * * * * /bin/sh -c 'cd /var/www/ && git pull origin master' > /var/www/autodeploy.txt'
 
